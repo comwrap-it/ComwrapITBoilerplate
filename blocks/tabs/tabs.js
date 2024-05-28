0817b47d7be47291dd876f1dfb59340f9addfc17
Ivan Viviani <ivan.viviani.vr@gmail.com>
2024-05-28 16:01:44 +0200

    feat: styled tabs block with accessible arrows handling

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -1,9 +1,11 @@
 // eslint-disable-next-line import/no-unresolved
 import { For, render } from 'solid-js/web';
+import { css } from 'solid-styled-components';
 import { toClassName } from '../../scripts/aem.js';
 import html from 'solid-js/html';
 import { container } from '../../styles/styles.js';
 import { createSignal } from 'solid-js';
+import { rem } from '../../scripts/utils.js';
 
 /**
  * @typedef {Object} Tab
@@ -15,6 +17,49 @@ import { createSignal } from 'solid-js';
 const TAB_BUTTON_ID_PREFIX = 'tab-';
 const TAB_PANEL_ID_PREFIX = 'tabpanel-';
 
+const CSS_TAB_LIST = css({
+    display: 'flex',
+    gap: rem(8),
+    maxWidth: '100%',
+    overflowX: 'auto',
+});
+
+const CSS_TAB_BUTTON = css({
+    flex: '0 0 max-content',
+    margin: 0,
+    border: '1px solid var(--dark-color)',
+    borderRadius: 0,
+    padding: `${rem(8)} ${rem(16)}`,
+    backgroundColor: 'var(--light-color)',
+    color: 'initial',
+    fontSize: 'unset',
+    fontWeight: 'bold',
+    lineHeight: 'unset',
+    textAlign: 'initial',
+    textOverflow: 'unset',
+    overflow: 'unset',
+    whiteSpace: 'unset',
+    transition: 'background-color 0.2s',
+    '&[aria-selected="true"]': {
+        borderBottom: '1px solid var(--background-color)',
+        backgroundColor: 'var(--background-color)',
+        cursor: 'initial',
+    },
+    '&[aria-selected="false"]:hover,&[aria-selected="false"]:focus': {
+        backgroundColor: 'var(--dark-color)',
+    },
+});
+
+const CSS_TAB_PANEL = css({
+    marginTop: rem(-1),
+    padding: `0 ${rem(16)}`,
+    border: '1px solid var(--dark-color)',
+    overflow: 'auto',
+    '&[aria-hidden="true"]': {
+        display: 'none',
+    },
+});
+
 /**
  * 
  * @param {{
@@ -24,15 +69,36 @@ const TAB_PANEL_ID_PREFIX = 'tabpanel-';
  */
 export const Tabs = ({ tabs }) => {
     const [activeTab, setActiveTab] = createSignal(0);
+    const handleKeyDownArrowsOnTabList = (/**@type {KeyboardEvent}*/e) => {
+        if (e.key !== 'ArrowRight' && e.key !== 'ArrowLeft') return;
+        // update active tab
+        switch (e.key) {
+            case 'ArrowRight':
+                setActiveTab((activeTab() + 1) % tabs.length);
+                break;
+            case 'ArrowLeft':
+                setActiveTab((activeTab() - 1 + tabs.length) % tabs.length);
+                break;
+            default:
+                break;
+        }
+        // focus active tab button
+        e.currentTarget.querySelectorAll('button')[activeTab()].focus();
+    };
 
     return html`<div class="${container}">
-        <div class="tab-list" role="tablist">
+        <div
+            class="${CSS_TAB_LIST}"
+            role="tablist"
+            onKeyDown="${handleKeyDownArrowsOnTabList}"
+        >
             <${For} each=${tabs}>
                 ${(t, i) => html`<button
-                    class="tabs-tab"
+                    class="${CSS_TAB_BUTTON}"
                     id="${TAB_BUTTON_ID_PREFIX+t.id}"
                     aria-controls="${TAB_PANEL_ID_PREFIX+t.id}"
                     aria-selected="${() => i() === activeTab()}"
+                    tabindex="${() => i() === activeTab() ? '0' : '-1'}"
                     type="button"
                     role="tab"
                     onClick="${() => setActiveTab(i())}"
@@ -43,7 +109,7 @@ export const Tabs = ({ tabs }) => {
         </div>
         <${For} each=${tabs}>
             ${(t, i) => html`<div
-                class="tabs-panel"
+                class="${CSS_TAB_PANEL}"
                 id="${TAB_PANEL_ID_PREFIX+t.id}"
                 aria-labelledby="${TAB_BUTTON_ID_PREFIX+t.id}"
                 aria-hidden="${() => i() !== activeTab()}"
