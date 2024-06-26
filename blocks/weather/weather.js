148610643a20bea69fc95f76b4a52d3138ff44f0
Gianluca Vasco <g.vasco@reply.it>
2024-06-26 10:35:25 +0200

    update weather block

--- blocks/weather/weather.js
+++ blocks/weather/weather.js
@@ -1,16 +1,115 @@
-import { WeatherApi } from "../../scripts/api/weather";
+import html from "solid-js/html";
+import { weatherApi } from "../../scripts/api/weather.js";
+
+/**
+ * Formats a date
+ * @param {string} date 'yyyy-mm-dd'
+ * @returns {string} a formatted date 'dd-mm-yyyy'
+ */
+export function formatDate(date) {
+    let dateArr = date.split('-');
+    dateArr = dateArr.reverse();
+    return dateArr.join('/');
+}
+
+/**
+ * Add 'https:' to icon path
+ * @param {string} iconPath 
+ * @returns {string} icon path with https protocol
+ */
+export function addHttps(iconPath) {
+    return "https:" + iconPath;
+}
 
 export default async function decorate(block) {
     // read block config
+    /**
+     * @type {{
+    *   title: string;
+    *   city: string;
+    *   numDays: string;
+    *   lang: string;
+    * }}
+    */
+   const config = [...block.children]
+       .map((row) => ([...row.children].map(col => col.innerText?.trim())))
+       .reduce((acc, [k,v]) => ({ ...acc, [k]: v }), {});
+
+    const title = config.title;
+    const city = config.city;
+    let numDays = 4;
+    try {
+        numDays = parseInt(config.numDays);
+    } catch(e) {
+        console.log(e);
+    }
+    const lang = config.lang;
 
     // empty block
     block.textContent = '';
 
     // call api
-    const weatherData = await WeatherApi.getForecast(/*... */);
+    const weatherData = await weatherApi.getForecast(city, numDays, lang);
+
+    // get current date and time
+    const time = weatherData.current.last_updated.split(' ');
+    const current_day = formatDate(time[0]);
+    const current_hour = time[1];
 
     // decorate html
-    block.append(`<div>
-        WEATHER
+    block.append(html`<div class="container">
+        <div class="title">
+            ${title}
+        </div>
+        <div class="city">
+            ${city}
+        </div>
+        <div class="today-container card">
+            <div class="day-text">${current_day}</div>
+            <div class="last-update">Ultimo aggiornamento: ${current_hour}</div>
+            <div class="weather-flex">
+                <img src="${addHttps(weatherData.current.condition.icon)}" alt="Weather data">
+                <div>${weatherData.current.temp_c}°C</div>
+            </div>
+            <div>${weatherData.current.condition.text}</div>
+        </div>
+
+        <div class="forecast-container">
+            <div class="forecast-item card">
+                <div class="day-text">${formatDate(weatherData.forecast.forecastday[1].date)}</div>
+                <div class="image-flex">
+                    <img src='${addHttps(weatherData.forecast.forecastday[1].day.condition.icon)}' alt="Weather data">
+                </div>
+                <div class="weather-flex">
+                    <div>Minima: ${weatherData.forecast.forecastday[1].day.mintemp_c}°C</div>
+                    <div>Massima: ${weatherData.forecast.forecastday[1].day.maxtemp_c}°C</div>
+                </div>
+                <div>${weatherData.forecast.forecastday[1].day.condition.text}</div>
+            </div>
+
+            <div class="forecast-item card">
+                <div class="day-text">${formatDate(weatherData.forecast.forecastday[2].date)}</div>
+                <div class="image-flex">
+                    <img src='${addHttps(weatherData.forecast.forecastday[2].day.condition.icon)}' alt="Weather data">
+                </div>
+                <div class="weather-flex">
+                    <div>Minima: ${weatherData.forecast.forecastday[2].day.mintemp_c}°C</div>
+                    <div>Massima: ${weatherData.forecast.forecastday[2].day.maxtemp_c}°C</div>
+                </div>
+                <div>${weatherData.forecast.forecastday[2].day.condition.text}</div>
+            </div>
+
+            <div class="forecast-item card">
+                <div class="day-text">${formatDate(weatherData.forecast.forecastday[3].date)}</div>
+                <div class="image-flex">
+                    <img src='${addHttps(weatherData.forecast.forecastday[3].day.condition.icon)}' alt="Weather data">
+                </div>
+                <div class="weather-flex">
+                    <div>Minima: ${weatherData.forecast.forecastday[3].day.mintemp_c}°C</div>
+                    <div>Massima: ${weatherData.forecast.forecastday[3].day.maxtemp_c}°C</div>
+                </div>
+                <div>${weatherData.forecast.forecastday[3].day.condition.text}</div>
+            </div>
+        </div>
     </div>`);
 }
--- scripts/api/weather.js
+++ scripts/api/weather.js
@@ -1,13 +1,44 @@
 /**
  * @typedef {Object} ForecastResponse
+ * @property {Current} current
  * @property {{
  *   forecastday: ForecastItem[],
  * }} forecast
  */
 
 /**
+ * @typedef {Object} Current
+ * @property {Condition} condition
+ * @property {string} last_updated
+ * @property {number} temp_c
+ */
+
+/**
  * @typedef {Object} ForecastItem
  * @property {string} date The date in 'yyyy-MM-dd' or 'yyyy-MM-dd hh:mm' format
+ * @property {Day} day
+ * @property {HourItem[]} hour
+ */
+
+/**
+ * @typedef {Object} Day
+ * @property {Condition} condition
+ * @property {number} mintemp_c
+ * @property {number} maxtemp_c
+ */
+
+/**
+ * @typedef {Object} Condition
+ * @property {string} text
+ * @property {string} icon
+ */
+
+/**
+ * @typedef {HourItem}
+ * @property {number} time_epoch
+ * @property {string} time
+ * @property {number} temp_c
+ * @property {Condition} condition
  */
 
 class WeatherApi {
@@ -19,7 +50,7 @@ class WeatherApi {
     };
 
     #buildEndpoint(endpoint) {
-        return `${this.#baseUrl}${endpoint}`;
+        return `https://${this.#baseUrl}${endpoint}`;
     }
 
     /**
@@ -27,14 +58,17 @@ class WeatherApi {
      * @param {string} cityName The city name in English
      * @param {number} numDays 
      * @param {string} lang 
-     * @returns {ForecastResponse}
+     * @returns {Promise<ForecastResponse>}
      */
     async getForecast(cityName, numDays, lang) {
         // compose URL
         const url = new URL(this.#buildEndpoint(this.#endpoints.forecast));
         // add query string params
-        url.searchParams.append('test','test');
+        url.searchParams.append('q', cityName);
+        url.searchParams.append('days', numDays);
+        url.searchParams.append('lang', lang);
         // add api key in query string
+        url.searchParams.append('key',this.#apiKey);
         
         // fetch request
         const resp = await fetch(url);
@@ -42,4 +76,4 @@ class WeatherApi {
     }
 }
 
-export const WeatherApi = new WeatherApi();
+export const weatherApi = new WeatherApi();
